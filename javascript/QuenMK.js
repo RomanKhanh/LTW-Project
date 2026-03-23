816bc7b43e09118489b66fc3e1c24b7bb172657a
anhloan7535 <anhloan200206@gmail.com>
2026-03-23 22:55:07 +0700

    Update QuenMK.js

--- javascript/QuenMK.js
+++ javascript/QuenMK.js
@@ -1,17 +1,18 @@
 function getPassword() {
+
   let contact = document.getElementById("contact").value.trim();
   let result = document.getElementById("result");
 
-  let savedUser = JSON.parse(localStorage.getItem("user"));
+  let user = JSON.parse(localStorage.getItem("user"));
 
-  if (!savedUser) {
-    result.innerText = "Chưa có tài khoản nào đăng ký";
+  if (!user) {
+    result.innerText = "Chưa có tài khoản nào";
     return;
   }
 
-  if (contact === savedUser.phone || contact === savedUser.email) {
-    result.innerText = "Mật khẩu của bạn là: " + savedUser.pass;
+  if (contact === user.email || contact === user.phone) {
+    result.innerText = "Mật khẩu của bạn: " + user.pass;
   } else {
-    result.innerText = "Không tìm thấy email hoặc số điện thoại này";
+    result.innerText = "Không tìm thấy email hoặc số điện thoại";
   }
 }
