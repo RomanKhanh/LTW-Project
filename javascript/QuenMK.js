eccdb830f7019aba02e5436f466dc88b91c83c01
anhloan7535 <anhloan200206@gmail.com>
2026-03-24 17:16:06 +0700

    Update giải thích code QuenMK.js

--- javascript/QuenMK.js
+++ javascript/QuenMK.js
@@ -1,18 +1,28 @@
+//lấy lại mật khẩu
 function getPassword() {
-
+  //lấy dữ dữ liệu từ người nhập
   let contact = document.getElementById("contact").value.trim();
+
+  //hiển thị thông báo. Đúng thì hiện mật khẩu, sai thì báo lỗi
   let result = document.getElementById("result");
 
-  let user = JSON.parse(localStorage.getItem("user"));
+  //lấy danh sách dữ liệu từ localStorage
+  let users = JSON.parse(localStorage.getItem("users")) || [];
 
-  if (!user) {
-    result.innerText = "Chưa có tài khoản nào";
+  //dùng .find để tìm user
+  let foundUser = users.find((u) => u.email === contact || u.phone === contact);
+
+  //nếu ô input bị bỏ trống
+  if (contact === "") {
+    result.innerText = "Vui lòng nhập email hoặc số điện thoại";
     return;
   }
 
-  if (contact === user.email || contact === user.phone) {
-    result.innerText = "Mật khẩu của bạn: " + user.pass;
-  } else {
+  //nếu không tìm thấy
+  if (!foundUser) {
     result.innerText = "Không tìm thấy email hoặc số điện thoại";
+  } else {
+    //tìm thấy thì hiện mật khẩu
+    result.innerText = "Mật khẩu của bạn: " + foundUser.pass;
   }
 }
